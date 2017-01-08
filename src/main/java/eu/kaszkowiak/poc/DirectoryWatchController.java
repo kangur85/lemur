e49370d361d55697ecb636c61c7942320b47c2f4
Krzysztof Kaszkowiak <krzysztof.kaszkowiak@gmail.com>
2017-01-08 23:17:14 +0100

    starting with Observables, making tests green (fake)

--- src/main/java/eu/kaszkowiak/poc/DirectoryWatchController.java
+++ src/main/java/eu/kaszkowiak/poc/DirectoryWatchController.java
@@ -1,11 +1,14 @@
 package eu.kaszkowiak.poc;
 
+import io.reactivex.Observable;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.file.Path;
+import java.util.LinkedList;
 import java.util.List;
+
 
 @RestController
 public class DirectoryWatchController {
@@ -14,8 +17,8 @@ public class DirectoryWatchController {
     @Value("${watchDirectoryPath}")
     private String watchDirectoryPath;
 
-    public List<Path> getAll() {
-        return null;
+    public Observable<List<Path>> getAll() {
+        return Observable.just(new LinkedList<Path>());
     }
 
 
